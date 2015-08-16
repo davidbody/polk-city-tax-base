58214499be1374fac6ec4ca394e98e571ee0abb7
David W. Body <davidbody@bigcreek.com>
2015-08-16 14:54:34 -0500

    Initial commit

---
+++ getData.R
@@ -0,0 +1,109 @@
+setwd("~/study/data_products/assessor-data")
+
+years <- 2008:2015
+
+for(year in years) {
+    dataUrl <- paste("http://web.assess.co.polk.ia.us/info/web/abstract/", year, "/AbstractReconciliationPolk", year, ".xls", sep="")
+    fileName <- paste("./data/AbstractReconciliationPolk", year, ".xls", sep="")
+    if (!file.exists(fileName)) {
+        print(paste("Fetching data for", year))
+        download.file(dataUrl, destfile=fileName, method="libcurl")
+    } else {
+        print(paste("We already have data for", year))
+    }
+}
+
+library(xlsx)
+
+readValues = function(year) {
+    fileName <- paste("./data/AbstractReconciliationPolk", year, ".xls", sep="")
+
+    agData  <- read.xlsx(fileName, 1, stringsAsFactors = FALSE)
+    resData <- read.xlsx(fileName, 3, stringsAsFactors = FALSE)
+    comData <- read.xlsx(fileName, 4, stringsAsFactors = FALSE)
+    indData <- read.xlsx(fileName, 6, stringsAsFactors = FALSE)
+
+#     print(paste("year", year, "\n"))
+
+    if (year %in% 2008:2013) {
+        agCities <- c(agData[34:52, 1], "Windsor Heights")
+        agValues <- c(as.numeric(agData[34:52, 5]), 0)
+#         print("agCities\n")
+#         print(agCities)
+#         print("\n")
+
+        resCities <- resData[35:54, 1]
+        resValues <- as.numeric(resData[35:54, 4])
+#         print("resCities\n")
+#         print(resCities)
+#         print("\n")
+
+        comCities <- comData[35:54, 1]
+        comValues <- as.numeric(comData[35:54, 4])
+#         print("comCities\n")
+#         print(comCities)
+#         print("\n")
+
+        indCities <- indData[34:53, 1]
+        indValues <- as.numeric(indData[34:53, 4])
+#         print("indCities\n")
+#         print(indCities)
+#         print("\n")
+    } else if (year %in% 2014:2015) {
+        agCities <- agData[49:68, 1]
+        agValues <- as.numeric(agData[49:68, 5])
+#         print("agCities\n")
+#         print(agCities)
+#         print("\n")
+
+        resCities <- resData[48:67, 1]
+        resValues <- as.numeric(resData[48:67, 4])
+#         print("resCities\n")
+#         print(resCities)
+#         print("\n")
+
+        comCities <- comData[48:67, 1]
+        comValues <- as.numeric(comData[48:67, 4])
+#         print("comCities\n")
+#         print(comCities)
+#         print("\n")
+
+        indCities <- indData[49:68, 1]
+        indValues <- as.numeric(indData[49:68, 4])
+#         print("indCities\n")
+#         print(indCities)
+#         print("\n")
+    } else {
+        stop(paste(year, "is not a valid year"))
+    }
+
+    df <- data.frame(city = agCities, year = year, residential = resValues, commercial = comValues, industrial = indValues, agricultural = agValues)
+    return(df)
+}
+
+df <- data.frame()
+for(year in years) {
+    df <- rbind(df, readValues(year))
+}
+
+df$total <- df$residential + df$commercial + df$industrial + df$agricultural
+
+plotCity <- function(df, city) {
+    library(reshape2)
+    library(ggplot2)
+    library(scales)
+
+    city.without_totals <- df[df$city == city, -7]
+
+    city.m <- melt(city.without_totals, id.vars = c("year", "city"))
+
+    g <- ggplot(city.m, aes(x = year, y = value, fill = variable))
+    g <- g + geom_bar(stat="identity")
+    g <- g + scale_y_continuous(labels = dollar)
+    g <- g + scale_x_continuous(breaks = years)
+    g <- g + labs(title = city, y = "Assessed Property Value", x = "Year")
+#     g <- g + annotate("text", x = year, y = 100000000, label = "90%")
+    return(g)
+}
+
+plotCity(df, "Polk City")
