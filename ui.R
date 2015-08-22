a8d0c2e0845903a142029bcc5eb78354bb3e7c96
David W. Body <davidbody@bigcreek.com>
2015-08-22 14:17:12 -0500

    Improved layout & added notes

--- ui.R
+++ ui.R
@@ -2,13 +2,18 @@ library(shiny)
 
 source("./getData.R")
 
-shinyUI(pageWithSidebar(
-    headerPanel("Assessed Property Values in Polk County, Iowa"),
-    sidebarPanel(
-        selectInput("city", "Select a City:", choices = cities(df), selected = "Polk City")
-    ),
+shinyUI(fluidPage(
+    titlePanel("Assessed Property Values in Polk County, Iowa"),
     mainPanel(
-        h3('Assessed Property Values'),
-        plotOutput("plot")
+        selectInput("city", "Select a City:", choices = cities(df), selected = "Polk City"),
+        plotOutput("plot"),
+        p('Percentages on the charts are residential property as a percentage of total valuation.'),
+        wellPanel(
+            HTML("<p>Notes:</p>"),
+            HTML("<p>Data source: Polk County Assessor <a href='http://web.assess.co.polk.ia.us/cgi-bin/web/tt/infoqry.cgi?tt=reconcile/reconcile_all'>web.assess.co.polk.ia.us/cgi-bin/web/tt/infoqry.cgi?tt=reconcile/reconcile_all</a></p>"),
+            HTML("<p>Includes only property located in Polk County.</p>"),
+            HTML("<p>Residential includes multiresidential class added in 2015.</p>")
+            )
+        )
     )
-))
+)
