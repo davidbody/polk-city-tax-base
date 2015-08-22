9165328bb2804b4a457c949c6ef7a49080df9472
David W. Body <davidbody@bigcreek.com>
2015-08-22 13:45:58 -0500

    UI tweaks

--- ui.R
+++ ui.R
@@ -3,9 +3,9 @@ library(shiny)
 source("./getData.R")
 
 shinyUI(pageWithSidebar(
-    headerPanel("Assessed Property for Cities in Polk County, Iowa"),
+    headerPanel("Assessed Property Values in Polk County, Iowa"),
     sidebarPanel(
-        selectInput("city", "Select a City:", choices = cities(df))
+        selectInput("city", "Select a City:", choices = cities(df), selected = "Polk City")
     ),
     mainPanel(
         h3('Assessed Property Values'),
